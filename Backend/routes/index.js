2f917bf4b1c1ede657e2f169c034bad850fc9942
ahsansaif47@ <ahsansaif47@hotmail.com>
2022-10-30 20:12:18 +0500

    ...

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // TODO: Return some blog posts...
+  // TODO: Return data about some on going campaigns.
+  // TODO: Return data to populate the homepage
+  // NOTE: Show some general camapign as well as specific campaigns,
+  // NOTE: Can display general statistics like areas where operational, total donations made etc.
 });
+
+router.get('/about', function (req, res, next) {
+  // TODO: If there is any data to display, then return it over this endpoint.
+  // NOTE: Some possible options are.. All the admins and superAdmin.. Thier profiles.
+  // NOTE: The catagory of campaigns supported by the organizations..
+  // NOTE: Have a mission statement, a goal and some other information.
+  // NOTE: Can display general statistics like areas where operational, total donations made etc. 
+});
+
+router.get('/', function (req, res, next) {
+
+})
 
 module.exports = router;
