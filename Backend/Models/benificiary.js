ff09f4133f9bf649357af457015e1df2ad889278
ahsansaif47@ <ahsansaif47@hotmail.com>
2022-11-11 11:59:29 +0500

    changes.

---
+++ Backend/Models/benificiary.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const validator = require("validator");
+
+
+const benificairySchema = mongoose.Schema({
+    id: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes("password")) {
+                throw new Error('Password cannot contain the word: "password"');
+            }
+        },
+    },
+    contact: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            let regex = '/[0-9]{11}/'
+            if (!regex.match(value)) {
+                throw new Error('Conatct cannot contain anything but Numbers')
+            }
+        }
+    },
+    requested_campaigns: [{}],
+    audit_reports: [{}],
+    account_no: {},
+    account_type: {}
+})
