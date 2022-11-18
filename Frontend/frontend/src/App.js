0c13e7eaa951b6b63fe6e0debe685e7220d76c7f
Syed Aown Raza <aown.raza963@gmail.com>
2022-11-18 23:09:54 +0500

    added topbar (theme not working)

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,9 +1,24 @@
+import {ColorModeContext, useMode} from './theme';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import Topbar from "./scenes/global/Topbar"
 
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
+  const [theme, colorMode] = useMode();
+  
+  return ( 
+    //Context for colormode, so we can use it anywhere.
+  <ColorModeContext.Provider value={colorMode}>
+    {/*ThemeProvider is used so that material UI has access to it as well.*/}
+    <ThemeProvider theme={theme}>
+    {/*CssBaseline resets the css to default on colorchange*/}
+      <CssBaseline/>
+      <div className="app">
+        <main className="content">
+          <Topbar/>
+        </main>
+      </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
--- Frontend/frontend/src/scenes/global/Topbar.jsx
+++ Frontend/frontend/src/scenes/global/Topbar.jsx
@@ -1,5 +1,63 @@
+import {Box, IconButton, useTheme} from "@mui/material";
+import { useContext } from "react";
+import {ColorModeContext, tokens} from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+
 const Topbar = () => {
-    return <div>Topbar</div>
+    //Grabs the theme in react using mui
+    const theme = useTheme()
+    //token for light/dark theme, goes to theme.js
+    const colors = tokens(theme.palette.mode)
+    //to allow to toggle states for colormode
+    const colorMode = useContext(ColorModeContext)
+
+    return(
+        //Box is basically div component from mui, but you can put CSS props in it
+        <Box display="flex" justifyContent="space-between" p={2}>
+            {/*Search Bar*/}
+            <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px" >
+                <InputBase sx={{ml: 2, flex: 1}} placeholder="Search"/>
+                <IconButton type="button" sx={{ p:1 }}>
+                    <SearchIcon/>
+                </IconButton>
+            </Box>
+
+            {/*Header Tags if needed*/}
+            <Box> 
+            </Box>
+
+
+            {/*Icons*/}
+            <Box display="flex"> 
+                <IconButton>
+                    <NotificationsOutlinedIcon/>
+                </IconButton>
+
+                <IconButton>
+                    <SettingsOutlinedIcon/>
+                </IconButton>
+
+                <IconButton>
+                    <PersonOutlinedIcon/>
+                </IconButton>
+
+                <IconButton onClick={colorMode.toggleColorMode}>
+                    {theme.palette.mode === "dark" ? (
+                        <DarkModeOutlinedIcon/>
+                    ) : (
+                        <LightModeOutlinedIcon/>
+                    )} 
+                </IconButton>
+            </Box>
+        </Box>
+    )
 }
 
 export default Topbar;
