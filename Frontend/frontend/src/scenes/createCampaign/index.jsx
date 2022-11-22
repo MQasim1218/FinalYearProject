917634e70fbfcbe6cbad31d25625fa2ca63e94dc
ahsansaif47@ <ahsansaif47@hotmail.com>
2022-11-22 21:05:17 +0500

    some changes.. hope not breaking

--- Frontend/frontend/src/scenes/createCampaign/index.jsx
+++ Frontend/frontend/src/scenes/createCampaign/index.jsx
@@ -3,57 +3,71 @@ import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
-
+import axios from "axios"
 
 //initializing all inputs with their keys
 const initialValues = {
-    campaignName: "",
-    campaignType: "",
-    campaignGoal: "",
-    campaignLocation: "",
-    campaignGenre: "",
-    campaignDescription: "",
-}
-
+  campaignName: "",
+  campaignType: "",
+  campaignGoal: "",
+  campaignLocation: "",
+  campaignGenre: "",
+  campaignDescription: "",
+};
 
 //schema for validation
 const userSchema = yup.object().shape({
-    campaignName: yup.string().required("Required"),
-    campaignType: yup.string().required("Required"),
-    campaignGoal: yup.string().required("Required"),
-    campaignLocation: yup.string().required("Required"),
-    campaignGenre: yup.string().required("Required"),
-    campaignDescription: yup.string().required("Required"),
-})
+  campaignName: yup.string().required("Required"),
+  campaignType: yup.string().required("Required"),
+  campaignGoal: yup.string().required("Required"),
+  campaignLocation: yup.string().required("Required"),
+  campaignGenre: yup.string().required("Required"),
+  campaignDescription: yup.string().required("Required"),
+});
 
 //force width to not go below 600px
 const CreateCampaign = () => {
-    const isNonMobile = useMediaQuery("(min-width:600px)")
+  const isNonMobile = useMediaQuery("(min-width:600px)");
 
-//on submit, all inputs are stored in values
-const handleFormSubmit = (values) => {
-        console.log(values)
-    }
+  //on submit, all inputs are stored in values
+  const handleFormSubmit = async (values) => {
+    console.log(values);
 
-    return (
+    let data = await axios.post("http://localhost:3000/", JSON.stringify(values))
+    JSON.parse(data)
+  };
+
+  return (
     <Box m="20px">
-        <Header title="CREATE CAMPAIGN" subtitle="Create A New Campaign"/>
+      <Header title="CREATE CAMPAIGN" subtitle="Create A New Campaign" />
 
-        <Formik onSubmit={handleFormSubmit} initialValues={initialValues} validationSchema={userSchema}>
-            {({values, errors, touched, handleBlur, handleChange, handleSubmit}) => (
-                <form onSubmit={handleSubmit}>
-                    <Box
-                    display="grid"
-                    gap="30px"
-                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-                    sx={{
-                        "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-                    }}>
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={userSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+              }}
+            >
               <TextField
                 fullWidth
                 variant="filled"
                 type="text"
-                label="Compaign Name"
+                label="Campaign Name"
                 onBlur={handleBlur}
                 onChange={handleChange}
                 value={values.campaignName}
@@ -123,8 +137,12 @@ const handleFormSubmit = (values) => {
                 onChange={handleChange}
                 value={values.campaignDescription}
                 name="campaignDescription"
-                error={!!touched.campaignDescription && !!errors.campaignDescription}
-                helperText={touched.campaignDescription && errors.campaignDescription}
+                error={
+                  !!touched.campaignDescription && !!errors.campaignDescription
+                }
+                helperText={
+                  touched.campaignDescription && errors.campaignDescription
+                }
                 sx={{ gridColumn: "span 4" }}
               />
             </Box>
@@ -134,13 +152,11 @@ const handleFormSubmit = (values) => {
                 Create New Campaign
               </Button>
             </Box>
-
-
-                </form>
-            )}
-        </Formik>
+          </form>
+        )}
+      </Formik>
     </Box>
-    )
-}
+  );
+};
 
-export default CreateCampaign
+export default CreateCampaign;
