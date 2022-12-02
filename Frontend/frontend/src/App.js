8478377eddffe5b4a709dacdf9cdc70641790eb8
ahsansaif47@ <ahsansaif47@hotmail.com>
2022-12-02 09:05:54 +0500

    ...

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,5 +1,5 @@
-import {ColorModeContext, useMode} from './theme';
-import {CssBaseline, ThemeProvider} from '@mui/material';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 import Topbar from "./scenes/global/Topbar"
 import Sidebar from "./scenes/global/Sidebar"
@@ -14,31 +14,31 @@ import DonorDashboard from './scenes/donorDashboard';
 
 function App() {
   const [theme, colorMode] = useMode();
-  
-  return ( 
-    //Context for colormode, so we can use it anywhere.
-  <ColorModeContext.Provider value={colorMode}>
 
-    {/*ThemeProvider is used so that material UI has access to it as well.*/}
-    <ThemeProvider theme={theme}>
-    {/*CssBaseline resets the css to default on colorchange*/}
-      <CssBaseline/>
-      <div className="app">
-        <Sidebar/>
-        <main className="content">
-          <Topbar/>
-          <Routes>
-            <Route path="/" element = { <DonorDashboard/>}/>
-            <Route path="/donations" element = { <Donations/>}/>
-            <Route path="/users" element = { <Users/>}/>
-            <Route path="/recent" element = { <Recent/>}/>
-            <Route path="/createCampaign" element = { <CreateCampaign/>}/>
-            <Route path="/graphs" element = { <Graphs/>}/>
-            <Route path="/geography" element = { <GeographyMap/>}/>
-          </Routes>
-        </main>
-      </div>
-    </ThemeProvider>
+  return (
+    //Context for colormode, so we can use it anywhere.
+    <ColorModeContext.Provider value={colorMode}>
+
+      {/*ThemeProvider is used so that material UI has access to it as well.*/}
+      <ThemeProvider theme={theme}>
+        {/*CssBaseline resets the css to default on colorchange*/}
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<DonorDashboard />} />
+              <Route path="/donations" element={<Donations />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/recent" element={<Recent />} />
+              <Route path="/createCampaign" element={<CreateCampaign />} />
+              <Route path="/graphs" element={<Graphs />} />
+              <Route path="/geography" element={<GeographyMap />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
     </ColorModeContext.Provider>
   );
 }
