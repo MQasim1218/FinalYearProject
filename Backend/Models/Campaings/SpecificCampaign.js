0c439f22c09b21856d5fc08284f095caad9a6fc4
ahsansaif47@ <ahsan_saif47@hotmail.com>
2023-05-10 13:05:08 +0500

    added functionality for storing documet urls for campaings

--- Backend/Models/Campaings/SpecificCampaign.js
+++ Backend/Models/Campaings/SpecificCampaign.js
@@ -7,6 +7,26 @@ let campaignSchema = mongoose.Schema({
     required_amount: { type: String, required: true },
     donated_amount: { type: Number, default: 0 },
     catagory: { type: String, required: true },
+
+    campaign_documents: [
+        {
+            // type: String,
+
+            // NOTE: Validation check to ensure that the gives thing is a url, will remove if it does not match our requirements.
+            type: String,
+            required: true,
+            validate: {
+                validator: url => {
+                    return /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+                        .test(url)
+                },
+
+                message: 'Invalid URL provided or a corner case 🧇🧇'
+            }
+
+        }
+    ],
+
     location: {
         type: {
             type: String, // Don't do `{ location: { type: String } }`
@@ -19,6 +39,8 @@ let campaignSchema = mongoose.Schema({
         }
     },
     benificiary: { type: mongoose.Types.ObjectId, ref: 'benificairy' },
+
+    // Below are the fields for campaign status...
     approved: { type: Boolean, required: true, default: false },
     completed: { type: Boolean, default: false },
     rejected: { type: Boolean, required: true, default: false },
--- Backend/routes/Campaigns/CampaignSpecRouter.js
+++ Backend/routes/Campaigns/CampaignSpecRouter.js
@@ -6,7 +6,7 @@
  *      3.  the new car is on te
  */
 // const SpecificCampaigns = require("../../Models/Campaings/SpecificCampaign")
-const SpecificCampaigns = require("../../Controllers/Campaings/SpecCampaignCntrlr")
+const SpecificCampaigns = require("../../Controllers/Campaigns/SpecCampaignCntrlr")
 const express = require('express')
 const router = express.Router()
 
@@ -16,6 +16,17 @@ const router = express.Router()
 
 
 router.get("/:id", SpecificCampaigns.GetOneCampaign)
-router.get("/", GetAllCampaigns)
+router.get("/", SpecificCampaigns.GetAllCampaigns)
+
+
+router.post("/appeal", SpecificCampaigns.AppealCampaign)
+
+/**
+ * ANCHOR: This route is only to be used to append the the list of documents already attached!
+ * Generally, the benif uploads the documents directly from the appeal page, not here, this is to aad furhter documents. 
+*/
+router.post("/upload_docs", SpecificCampaigns.UploadDocuments)
+// * router.get("appealed status?? because this is visiable in multiple placess")
+
 
 module.exports = router
